ef84b21201e5a657d90d073324e212fdc23c0307
Santiago Suarez <sansuareze@gmail.com>
2023-05-24 15:40:57 +0200

    functionalities done but css/bootstrap pending

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants"
+Restaurant.create(name: "Restaurant 1", address: "123 Main St", phone_number: "555-555-5555", category: "italian")
+Restaurant.create(name: "Restaurant 2", address: "456 Oak St", phone_number: "555-555-5555", category: "mexican")
+Restaurant.create(name: "Restaurant 3", address: "789 Maple St", phone_number: "555-555-5555", category: "american")
+Restaurant.create(name: "Restaurant 4", address: "987 Elm St", phone_number: "555-555-5555", category: "chinese")
+Restaurant.create(name: "Restaurant 5", address: "654 Pine St", phone_number: "555-555-5555", category: "japanese")
+
+puts "restaurants created"
